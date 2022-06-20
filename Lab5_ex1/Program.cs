77262baaac1fccff623d5c5fae9e42bbf7d082bd
Арсений Стефаненко <goloologo@gmail.com>
2022-06-21 01:07:28 +0300

    21.06.22

--- Lab5_ex1/Program.cs
+++ Lab5_ex1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lab5_ex1
 {
@@ -6,7 +7,49 @@ namespace Lab5_ex1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var websites = new Stack<string>();
+            var oldweb = new Stack<string>();
+            websites.Push("http://www.asm.org");
+            while (true)
+            {
+                string cmd = Console.ReadLine();
+                if (cmd == "back")
+                {
+                    if (websites.Count == 1)
+                    {
+                        Console.WriteLine("Ignored");
+                    }
+                    else
+                    {
+                        websites.Pop();
+                        Console.WriteLine(websites.Peek());
+                    }
+                }
+                else if (cmd == "forward")
+                {
+                    if (oldweb.Count == 0)
+                    {
+                        Console.WriteLine("Ignored");
+                    }
+                    else
+                    {
+                        websites.Push(oldweb.Pop());
+                        Console.WriteLine(websites.Peek());
+                    }
+                }
+                else if (cmd == "visit")
+                {
+                    string website = Console.ReadLine();
+                    websites.Push(website);
+                    oldweb.Push(website);
+                    Console.WriteLine(website);
+                }
+                else if (cmd == "quit")
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
